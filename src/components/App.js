ac26c738efc887ca4fb8e3550a2bc38ccdadd983
jkgrim <j.grimshaw37@gmail.com>
2022-07-17 21:23:16 -0600

    restructured skeleton

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Child from "./Child";
+
+function App() {
+  return (
+    <div className="header-wrapper">
+      <Child fName="Jacob" lName="Grimshaw" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Child.js
@@ -0,0 +1,3 @@
+export default function Child(props) {
+  return <h1>Hello, my name is {props.fName + " " + props.lName}</h1>;
+}
